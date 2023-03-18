86442f83155ff569200f74fd3f4a679195f4a430
rzhestkov <127289050+rzhestkov@users.noreply.github.com>
2023-03-18 15:12:50 +0300

    Update SetDate.ps1

--- SetDate.ps1
+++ SetDate.ps1
@@ -16,6 +16,10 @@ $newCaptureDate="2004:05:15"
 #Час м минута, от которых будут считаться новые часы и минуты (обычно не нужно изменять)
 $startCaptureHour=12 # нужно чтобы конечный час не стал больше 23
 $startCaptureMin=1 # от 1 до 58
+$startCaptureSec=5 # от 1 до 58. 5 - оптимальное значение
+$SecAcuracy=1 # если "0", то прибаляет по минуте, если "1", то по 5 секунд
+$OverwriteOriginal=0 # если "0", то оригинальные файлы будут сохранены рядом, с расширением вида "jpg_original", если не ноль, то оригинальные файлы будут перезаписаны
+#------------------------------------------------------------------
 
 Write-Host 'Starting .....'
 #Начальные значения переменных
@@ -31,6 +35,16 @@ if (-not((IsFileExists($cur_path+'\exiftool.exe')))) {
 #------------------------------------------------------------------
 #Основной текст программы
 #------------------------------------------------------------------
+if ($SecAcuracy -eq 0) { # сообщение
+    write-host 'Capture time increment in files will be set by ' -NoNewline
+    write-host -ForegroundColor Green '1 minute'
+} elseif ($SecAcuracy -eq 1) {
+    write-host 'Capture time increment in files will be set by ' -NoNewline
+    write-host -ForegroundColor Green '5 secodns'
+} else {
+    write-host -ForegroundColor Red 'An invalid value was set in a variable $SecAcuracy'
+}
+
 #Просматриваем текущую папку и берем по очереди файлы картинок
 #обход файлов
 $FileList= Get-ChildItem -Path $cur_path -Force -ErrorAction SilentlyContinue -File -Name -Include @("*.psd", "*.jpeg", "*.jpg", "*.arw")
@@ -45,17 +59,35 @@ foreach ($FileSpec in $FileList) {
     write-host 'Found file : ' -NoNewline
     write-host -ForegroundColor Green $FileSpec -NoNewline
     #Формирование строки с параметрами
-    $newCaptureHour=$startCaptureHour +[math]::Floor(($startCaptureMin+($numOfFiles-1))/60)
-    $newCaptureHour=([String]$newCaptureHour).PadLeft(2, '0') #добавление ведущего нуля
-    $newCaptureMin=$startCaptureMin+($numOfFiles-1)-[math]::Floor(($startCaptureMin+($numOfFiles-1))/60)*60
-    $newCaptureMin=([String]$newCaptureMin).PadLeft(2, '0') #добавление ведущего нуля
-    $commandString='-AllDates="'+$newCaptureDate+' '+$newCaptureHour+':'+$newCaptureMin+':00" '+ $FileSpec
+    if ($SecAcuracy -eq 0) { # прибавляем по минуте к дате съемки у каждого нового файла
+        $newCaptureHour=$startCaptureHour +[math]::Floor(($startCaptureMin+($numOfFiles-1))/60)
+        $newCaptureHour=([String]$newCaptureHour).PadLeft(2, '0') #добавление ведущего нуля
+        $newCaptureMin=$startCaptureMin+($numOfFiles-1)-[math]::Floor(($startCaptureMin+($numOfFiles-1))/60)*60
+        $newCaptureMin=([String]$newCaptureMin).PadLeft(2, '0') #добавление ведущего нуля
+        $commandString='-AllDates="'+$newCaptureDate+' '+$newCaptureHour+':'+$newCaptureMin+':00" '
+    } elseif ($SecAcuracy -eq 1) { # прибавляем по 5 секунд к дате съемки у каждого нового файла
+        $newCaptureSec=($startCaptureSec+($numOfFiles-1)*5)-[math]::Floor(($startCaptureSec+($numOfFiles-1)*5)/60)*60
+        $newCaptureSec=([String]$newCaptureSec).PadLeft(2, '0') #добавление ведущего нуля
+
+        $newCaptureMin=$startCaptureMin+[math]::Floor(($numOfFiles-1)*5/60+0.00001) # здесь 0.00001 - это костыль, чтобы правильнее работало округление
+        $newCaptureMin=([String]$newCaptureMin).PadLeft(2, '0') #добавление ведущего нуля
+
+        $newCaptureHour=$startCaptureHour+[math]::Floor(($numOfFiles-1)*5/3600+0.00001)
+        $newCaptureHour=([String]$newCaptureHour).PadLeft(2, '0') #добавление ведущего нуля
+
+        $commandString='-AllDates="'+$newCaptureDate+' '+$newCaptureHour+':'+$newCaptureMin+':'+$newCaptureSec+'" '
+    }
+    # доработка строки с параметрами
+    if ($OverwriteOriginal -eq 0) {
+        $commandString=$commandString + $FileSpec
+    } else {
+        $commandString=$commandString + '-overwrite_original ' + $FileSpec
+    }
     write-host ' set parameter: '$commandString
     # пример    -AllDates="1998:02:01 12:01:00"' IMG_199802.01.psd  
     #------------------------------------------------------------------
-    #здесь вся работа происходит
+    #здесь вся происходит изменение файла
     #------------------------------------------------------------------
-    #Start-Process -FilePath  .\ffmpeg.exe  -ArgumentList $transcodeString -Wait -NoNewWindow -RedirectStandardError $ffmpegfilename
     Start-Process -FilePath  .\exiftool.exe  -ArgumentList $commandString -Wait -NoNewWindow
 
     #------------------------------------------------------------------
